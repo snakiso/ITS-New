d508d89fd5a20a2de49cc01944515e99b5cf1f52
snakiso <igorchagov@mail.ru>
2023-08-09 18:13:24 +0300

    add new blocks

--- js/header.js
+++ js/header.js
@@ -1,15 +1,29 @@
 let dropdown = document.querySelector('.header__dropdown');
 let dropdownBtn = document.querySelector('.header__item_dropdown');
 let hamburger = document.querySelector('.hamburger')
+let body = document.querySelector('.body')
 let menu = document.querySelector('.menu')
 
 dropdownBtn.addEventListener("click", () => {
   dropdown.classList.toggle('active')
 })
 
+dropdownBtn.addEventListener("mouseover", () => {
+  dropdown.classList.add('active')
+})
+
+dropdown.addEventListener("mouseout", () => {
+  dropdown.classList.remove('active')
+})
+
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('is-active')
   if (menu) {
     menu.classList.toggle('is-active')
+  }
+  if (menu.classList.contains('is-active')) {
+    body.style.overflow = "hidden"
+  } else {
+    body.style.overflow = "scroll"
   }
 })
